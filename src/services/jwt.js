f22f178a9c8023875d00810bbb797ddc68379f8b
Diego Patzan <diegopatzan24@gmail.com>
2026-01-27 10:36:12 -0600

    chore: add proprietary license notice to multiple files and update LICENSE with governing law clause

--- src/services/jwt.js
+++ src/services/jwt.js
@@ -1,3 +1,13 @@
+/**
+ * Copyright (c) 2026 Diego Patzán. All Rights Reserved.
+ * 
+ * This source code is licensed under a Proprietary License.
+ * Unauthorized copying, modification, distribution, or use of this file,
+ * via any medium, is strictly prohibited without express written permission.
+ * 
+ * For licensing inquiries: GitHub @dpatzan
+ */
+
 const jwt_simple = require('jwt-simple')
 const moment = require('moment')
 const secret = process.env.JWT_SECRET || 'clave_secreta'
